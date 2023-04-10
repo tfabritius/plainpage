09f6ca2fb126546429d40043be333f4179156267
Thomas Fabritius <tfabritius@gmail.com>
2023-04-10 10:39:38 +0200

    Avoid auth errors for /_api/app

--- backend/server/server.go
+++ backend/server/server.go
@@ -79,11 +79,11 @@ func (app App) GetHandler() http.Handler {
 		AllowedHeaders: []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
 	}))
 
+	r.Get("/_api/app", app.exposeConfig)
+
 	r.
 		With(app.Token.Token2ContextMiddleware).
 		Route("/_api", func(r chi.Router) {
-			r.Get("/app", app.exposeConfig)
-
 			r.With(app.RequireAppPermission(storage.AccessOpAdmin)).Get("/config", app.getConfig)
 			r.With(app.RequireAppPermission(storage.AccessOpAdmin)).Patch("/config", app.patchConfig)
 

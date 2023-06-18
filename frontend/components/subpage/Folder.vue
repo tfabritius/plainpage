13d8624a4e0d60baa269d02be8e0b15a265b70d7
Thomas Fabritius <tfabritius@gmail.com>
2023-06-18 11:54:24 +0200

    Display content name if title is empty in folder listing

--- frontend/components/subpage/Folder.vue
+++ frontend/components/subpage/Folder.vue
@@ -254,7 +254,7 @@ onKeyStroke('Backspace', (e) => {
       <div v-for="entry of folder.content.filter(e => e.isFolder)" :key="entry.name">
         <NuxtLink v-slot="{ navigate, href }" :to="entry.url" custom>
           <ElLink :href="href" @click="navigate">
-            <Icon name="ci:folder" class="mr-1" /> {{ entry.title }}
+            <Icon name="ci:folder" class="mr-1" /> {{ entry.title || entry.name }}
           </ElLink>
         </NuxtLink>
       </div>
@@ -268,7 +268,7 @@ onKeyStroke('Backspace', (e) => {
       <div v-for="entry of folder.content.filter(e => !e.isFolder)" :key="entry.name">
         <NuxtLink v-slot="{ navigate, href }" :to="entry.url" custom>
           <ElLink :href="href" @click="navigate">
-            {{ entry.title }}
+            {{ entry.title || entry.name }}
           </ElLink>
         </NuxtLink>
       </div>

7911a1ebc149a4d326a47762c30e64125ab3a10b
Thomas Fabritius <tfabritius@gmail.com>
2023-06-25 12:20:15 +0200

    Show loading animation while hydrating

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   ssr: false,
+  spaLoadingTemplate: 'spa-loading-template.html',
 
   modules: [
     '@element-plus/nuxt',

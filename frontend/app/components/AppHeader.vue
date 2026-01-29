a4f618f7bf21e5535ab83e09e4af4322dc64fea9
Thomas Fabritius <tfabritius@gmail.com>
2026-01-29 22:13:08 +0100

    Enlarge search field when focussed

--- frontend/app/components/AppHeader.vue
+++ frontend/app/components/AppHeader.vue
@@ -38,6 +38,8 @@ const route = useRoute()
 const toast = useToast()
 
 const searchQuery = ref('')
+const searchFocused = ref(false)
+
 async function onSearch() {
   await navigateTo({ path: '/_search', query: { q: searchQuery.value } })
   searchQuery.value = ''
@@ -139,31 +141,37 @@ const menuItems = computed(() => {
 </script>
 
 <template>
-  <div class="flex justify-between">
-    <ULink to="/" :active="false" class="text-xl font-light flex items-center whitespace-nowrap">
+  <div class="flex items-center">
+    <!-- Section 1: Title (fixed, no shrink) -->
+    <ULink to="/" :active="false" class="text-xl font-light flex items-center whitespace-nowrap flex-shrink-0">
       <!-- favicon -->
       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" class="w-[1em] h-[1em]"><path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 3H8.2c-1.12 0-1.68 0-2.108.218a1.999 1.999 0 0 0-.874.874C5 4.52 5 5.08 5 6.2v11.6c0 1.12 0 1.68.218 2.108a2 2 0 0 0 .874.874c.427.218.987.218 2.105.218h7.606c1.118 0 1.677 0 2.104-.218c.377-.192.683-.498.875-.874c.218-.428.218-.986.218-2.104V9m-6-6c.286.003.466.014.639.055c.204.05.399.13.578.24c.202.124.375.297.72.643l3.126 3.125c.346.346.518.518.642.72c.11.18.19.374.24.578c.04.173.051.354.054.639M13 3v2.8c0 1.12 0 1.68.218 2.108a2 2 0 0 0 .874.874c.427.218.987.218 2.105.218h2.802m0 0H19" /></svg>
       <span>{{ appTitle }}</span>
     </ULink>
 
-    <span class="flex">
+    <!-- Section 2: Middle box with search (flexible, grows/shrinks) -->
+    <div class="flex-1 flex justify-end min-w-0 mr-1 ml-4">
       <UInput
         v-model="searchQuery"
         :placeholder="$t('search')"
-        class="max-w-40 mx-1"
+        :class="searchFocused ? 'w-full' : 'w-40'"
+        class="min-w-0 transition-[width] duration-200 ease-in-out"
         size="xs"
         trailing-icon="tabler:search"
+        @focus="searchFocused = true"
+        @blur="searchFocused = false"
         @keypress.enter="onSearch"
         @keydown="onKeyPressInSearch"
       />
+    </div>
 
-      <UDropdownMenu
-        class="m-1"
-        :items="menuItems"
-        size="lg"
-      >
-        <ReactiveButton icon="tabler:menu-2" variant="link" :label="t('menu')" />
-      </UDropdownMenu>
-    </span>
+    <!-- Section 3: Menu (fixed, no shrink) -->
+    <UDropdownMenu
+      class="flex-shrink-0 m-1"
+      :items="menuItems"
+      size="lg"
+    >
+      <ReactiveButton icon="tabler:menu-2" variant="link" :label="t('menu')" />
+    </UDropdownMenu>
   </div>
 </template>

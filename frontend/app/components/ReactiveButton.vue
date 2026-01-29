2291dcf561c7b6b5b223d71831557da670d0b9d1
Thomas Fabritius <tfabritius@gmail.com>
2026-01-29 22:19:18 +0100

    Fix height of reactive button

--- frontend/app/components/ReactiveButton.vue
+++ frontend/app/components/ReactiveButton.vue
@@ -12,10 +12,10 @@ defineProps<{
 //
 // original style of md size button: base: 'px-2.5 py-1.5 text-sm gap-1.5',
 //
-// height of icon + text: 20px + 5 px padding top/bottom = 30px
-// height of icon only:   14px + 8 px padding top/bottom = 30px
+// height of icon + text: 22px + 5 px padding top/bottom = 32px
+// height of icon only:   20px + 6 px padding top/bottom = 32px
 //
-// --> base: 'py-[8px] md:py-[5px]',
+// --> base: 'py-[6px] md:py-[5px]',
 //
 // add some additional horizontal padding for the icon on small screens:
 // --> base: 'px-4 md:px-2.5'
@@ -27,7 +27,7 @@ defineProps<{
     :label="label"
     :ui="{
       label: 'hidden md:inline',
-      base: 'px-4 md:px-2.5 py-[8px] md:py-[5px]',
+      base: 'px-4 md:px-2.5 py-[6px] md:py-[5px]',
     }"
   />
 </template>
